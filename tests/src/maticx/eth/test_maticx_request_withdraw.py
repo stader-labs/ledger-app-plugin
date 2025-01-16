7c8b4c625ff8c84fcf3957f007c9126c35b6bfa5
Sergey <2901744+evercoinx@users.noreply.github.com>
2025-01-16 15:44:51 +0100

    Update transaction addresses for some tests

--- tests/src/maticx/eth/test_maticx_request_withdraw.py
+++ tests/src/maticx/eth/test_maticx_request_withdraw.py
@@ -5,10 +5,10 @@ contract = load_contract(
     "0xD22dd194B8ec2abEeeFAC36a69cd8898FB6C43b3"
 )
 
-# Test from replayed transaction: https://etherscan.io/tx/0x718110ca67a3f440195c388c16a1749e0d89877817d2d3e944365855f642145d
+# Test from replayed transaction: https://etherscan.io/tx/0x04727942385b723c4dc4881d696a3cb5ad576283231697d7786905712a0c1fa2
 
 def test_maticx_request_withdraw(backend, firmware, navigator, test_name, wallet_addr):
-    data = "0x745400c9000000000000000000000000000000000000000000000045c41fde9b3d420000"
+    data = "0x745400c90000000000000000000000000000000000000000000000001bc16d674ec80000"
     run_test(
         contract, 
         data, 
--- tests/src/maticx/eth/test_maticx_submit_pol.py
+++ tests/src/maticx/eth/test_maticx_submit_pol.py
@@ -5,10 +5,10 @@ contract = load_contract(
     "0xD22dd194B8ec2abEeeFAC36a69cd8898FB6C43b3"
 )
 
-# Test from replayed transaction: https://etherscan.io/tx/0x92f606f3ab623de706e4a1a2bfdcfa95d07f071f2f3aacfc729aefa6b3c6d94c
+# Test from replayed transaction: https://etherscan.io/tx/0x0a75cc89e54f34610a3064c607205b7909a1f4cad80e11646732b2338fc4bf16
 
 def test_maticx_submit_pol(backend, firmware, navigator, test_name, wallet_addr):
-    data = "0x1564da790000000000000000000000000000000000000000000000481591030b2a830000"
+    data = "0x1564da7900000000000000000000000000000000000000000000001bc16d674ec80000"
     run_test(
         contract, 
         data, 
